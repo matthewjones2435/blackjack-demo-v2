2a98da15d80c6a48301894519439c6cd8dc5d0a3
Nick Bennett <nick@nickbenn.com>
2019-06-20 19:02:52 -0600

    Move web service tasks to interface.

--- app/src/main/java/edu/cnm/deepdive/blackjackdemo/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/blackjackdemo/controller/MainActivity.java
@@ -2,24 +2,21 @@ package edu.cnm.deepdive.blackjackdemo.controller;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import edu.cnm.deepdive.android.FluentAsyncTask;
 import edu.cnm.deepdive.blackjackdemo.R;
 import edu.cnm.deepdive.blackjackdemo.model.Card;
 import edu.cnm.deepdive.blackjackdemo.model.Deck;
-import edu.cnm.deepdive.blackjackdemo.model.Draw;
 import edu.cnm.deepdive.blackjackdemo.model.Hand;
-import edu.cnm.deepdive.blackjackdemo.service.DeckOfCardsService;
+import edu.cnm.deepdive.blackjackdemo.service.DeckOfCardsService.CreateDeckTask;
+import edu.cnm.deepdive.blackjackdemo.service.DeckOfCardsService.DrawCardsTask;
+import edu.cnm.deepdive.blackjackdemo.service.DeckOfCardsService.ShuffleDeckTask;
 import edu.cnm.deepdive.blackjackdemo.view.HandAdapter;
-import java.io.IOException;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
   private Deck deck;
   private Hand hand;
-  private DeckOfCardsService service;
   private RecyclerView handView;
   private HandAdapter handAdapter;
 
@@ -38,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     setupToolbar();
     setupFloatingActionButton();
     setupRecyclerView();
-    setupService();
     setupDeck();
   }
 
@@ -83,41 +78,19 @@ public class MainActivity extends AppCompatActivity {
     handView.setLayoutManager(new LinearLayoutManager(this));
   }
 
-  private void setupService() {
-    Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl(getString(R.string.base_url))
-        .addConverterFactory(GsonConverterFactory.create())
-        .build();
-    service = retrofit.create(DeckOfCardsService.class);
-  }
-
   private void setupDeck() {
-    new FluentAsyncTask<Integer, Void, Void>()
-        .setPerformer((values) -> {
-          int decksInShoe = values[0];
-          try {
-            deck = service.newDeck(decksInShoe).execute().body();
-            return null;
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        })
-        .setOnSuccessListener((ignore) -> setupHand())
+    new CreateDeckTask()
+        .setOnCompletionListener((deck) -> this.deck = deck)
+        .setOnSuccessListener((ignored) -> setupHand())
         .execute(DECKS_IN_SHOE);
   }
 
   private void shuffleDeck() {
-    new FluentAsyncTask<Void, Void, Void>()
-        .setPerformer((values) -> {
-          try {
-            service.shuffle(deck.getId()).execute();
-            return null;
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        })
-        .setOnSuccessListener((ignore) -> setupHand())
-        .execute();
+    new ShuffleDeckTask()
+        .setOnSuccessListener(
+            (ignored) -> setupHand()
+        )
+        .execute(deck);
   }
 
   private void setupHand() {
@@ -128,15 +101,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void drawCards(int numCards) {
-    new FluentAsyncTask<Integer, Void, Draw>()
-        .setPerformer((values) -> {
-          int cardsToDraw = values[0];
-          try {
-            return service.draw(deck.getId(), cardsToDraw).execute().body();
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
-        })
+    new DrawCardsTask(deck)
         .setOnSuccessListener((draw) -> {
           int startPosition = hand.getCards().size();
           for (Card card : draw.getCards()) {
--- app/src/main/java/edu/cnm/deepdive/blackjackdemo/service/DeckOfCardsService.java
+++ app/src/main/java/edu/cnm/deepdive/blackjackdemo/service/DeckOfCardsService.java
@@ -1,8 +1,14 @@
 package edu.cnm.deepdive.blackjackdemo.service;
 
+import androidx.annotation.Nullable;
+import edu.cnm.deepdive.android.FluentAsyncTask;
+import edu.cnm.deepdive.blackjackdemo.BuildConfig;
 import edu.cnm.deepdive.blackjackdemo.model.Deck;
 import edu.cnm.deepdive.blackjackdemo.model.Draw;
+import java.io.IOException;
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -17,5 +23,75 @@ public interface DeckOfCardsService {
 
   @GET("deck/{deckId}/shuffle")
   Call<Deck> shuffle(@Path("deckId") String deckId);
-  
+
+  class InstanceHolder {
+
+    private static final DeckOfCardsService INSTANCE;
+
+    static {
+      Retrofit retrofit = new Retrofit.Builder()
+          .baseUrl(BuildConfig.BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create())
+          .build();
+      INSTANCE = retrofit.create(DeckOfCardsService.class);
+    }
+
+  }
+
+  class CreateDeckTask extends FluentAsyncTask<Integer, Void, Deck> {
+
+    @Nullable
+    @Override
+    protected Deck perform(Integer... values) {
+      Deck deck = null;
+      int decksInShoe = values[0];
+      try {
+        deck = InstanceHolder.INSTANCE.newDeck(decksInShoe).execute().body();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      return deck;
+    }
+
+  }
+
+  class ShuffleDeckTask extends FluentAsyncTask<Deck, Void, Void> {
+
+    @Nullable
+    @Override
+    protected Void perform(Deck... decks) {
+      try {
+        InstanceHolder.INSTANCE.shuffle(decks[0].getId()).execute();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      return null;
+    }
+
+  }
+
+  class DrawCardsTask extends FluentAsyncTask<Integer, Void, Draw> {
+
+    private Deck deck;
+
+    public DrawCardsTask(Deck deck) {
+      this.deck = deck;
+    }
+
+    @Nullable
+    @Override
+    protected Draw perform(Integer... values) {
+      int cardsToDraw = values[0];
+      Draw draw = null;
+      try {
+        draw = InstanceHolder.INSTANCE.draw(deck.getId(), cardsToDraw).execute().body();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      return draw;
+
+    }
+
+  }
+
 }
